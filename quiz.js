01637f024f4f8bc9efb5c89be6577c5914ba6bb1
Martins Liyansan <liyansanm@gmail.com>
2025-08-02 23:47:57 +0100

    corrected commit

--- quiz.js
+++ quiz.js
@@ -2,19 +2,24 @@ function checkAnswer() {
   const correctAnswer = "4";
 
   const selectedRadioButton = document.querySelector('input[name="quiz"]:checked');
-  const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+  let userAnswer;
+
+  if (selectedRadioButton) {
+    userAnswer = selectedRadioButton.value;
+  }
 
   const feedbackElement = document.getElementById("feedback");
 
-   
+  if (userAnswer) {
     if (userAnswer === correctAnswer) {
       feedbackElement.textContent = "Correct! Well done.";
     } else {
       feedbackElement.textContent = "That's incorrect. Try again!";
     }
-  
+  } else {
+    feedbackElement.textContent = "Please select an answer.";
   }
-
+}
 
 const submitButton = document.getElementById("submit-answer");
 submitButton.addEventListener("click", checkAnswer);
